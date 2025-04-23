141bb643b2ab46147143a1993db3c426aef155ac
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-04-23 20:32:59 +0200

    intro

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,9 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className='flex flex-col min-h-screen'>
-        <header></header>
         <main className='grow'>{children}</main>
-        <footer></footer>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div className='text-red-600'>Test</div>;
+  return (
+    <section className='p-5 flex flex-col gap-5'>
+      <h1 className='font-serif font-bold text-2xl text-center'>
+        My Frontend Mentor Portfolio
+      </h1>
+      <p>
+        All components and webpages has been created in Next.js,
+        components or pages containing data that can be interacted
+        with utilizes Appwrite to handle backend and API.
+      </p>
+    </section>
+  );
 }
