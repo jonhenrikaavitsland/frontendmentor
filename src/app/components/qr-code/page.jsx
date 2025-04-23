ca70b57d99e5ba11d2e6c182a38705bf6c0b3558
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-04-23 22:42:20 +0200

    hidden h1

--- src/app/components/qr-code/page.jsx
+++ src/app/components/qr-code/page.jsx
@@ -3,6 +3,7 @@ import QrCode from '@/components/qr-code';
 export default function Page() {
   return (
     <div className='bg-slate-300 grow flex justify-center items-center'>
+      <h1 className='sr-only'>qr-code component</h1>
       <QrCode />
     </div>
   );
